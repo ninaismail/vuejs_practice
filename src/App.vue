dc40ee3be20203298c62784d60394784f0095f14
Najat Ismail <46082009+ninaismail@users.noreply.github.com>
2024-03-26 15:58:48 +0200

    tailwind

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header class="bg-red-700">
+  <!-- <header class="w-10/12 flex justify-between items-center mx-auto">
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
@@ -17,5 +17,10 @@ import HelloWorld from './components/HelloWorld.vue'
     </div>
   </header>
 
-  <RouterView />
+  <RouterView /> -->
+
+  <section id="banner" class="w-full pt-[70px] pb-2 lg:h-screen h-full max-h-screen mx-auto flex flex-wrap items-center justify-center">
+    <h1 class="uppercase text-[30px] lg:text-4xl 2xl:text-5xl text-center font-bold">Lorem Ipsun <span class="font-light">Dollor Sit</span></h1>
+  </section>
+
 </template>
